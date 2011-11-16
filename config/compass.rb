8809df44952ef95a0081abf21d43f6bde2c9f811
unknown <Yura@KVAZAR-Yura.(none)>
2011-11-16 13:26:17 +0200

    тренировлся

--- config/compass.rb
+++ config/compass.rb
@@ -1,10 +1,14 @@
 # This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
 require 'compass-bootstrap'
-project_type = :rails
+#project_type = :rails
 
 # Set this to the root of your project when deployed:
 http_path = "/"
+css_dir = "public/stylesheets"
+sass_dir = "public/stylesheets/sass"
+images_dir = "public/images"
+javascripts_dir = "public/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
